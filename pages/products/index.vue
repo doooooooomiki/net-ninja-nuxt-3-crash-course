4ebad8bdf6b38815e35d65ab980a53514ade705d
Dominic <dominic.kirste@gmail.com>
2022-12-04 12:01:28 +0100

    added products/index.vue

---
+++ pages/products/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2>Products</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
+      voluptates voluptatem voluptatibus. Veniam excepturi ea quam tempore vero
+      doloribus quibusdam!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
+      voluptates voluptatem voluptatibus. Veniam excepturi ea quam tempore vero
+      doloribus quibusdam!
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
+</style>
