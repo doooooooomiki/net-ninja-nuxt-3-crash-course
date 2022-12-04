5adb10d46b2556445f83bc06afa98eb362de13a4
Dominic <dominic.kirste@gmail.com>
2022-12-04 13:04:20 +0100

    added tailwind + styles

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,23 @@
 <template>
   <div>
-    <header>
-      <DojoNavigation></DojoNavigation>
+    <header class="shadow-sm bg-white">
+      <nav class="container mx-auto p-4 flex justify-between">
+        <NuxtLink to="/" class="font-bold">Nuxt Dojo</NuxtLink>
+        <ul class="flex gap-4">
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/about">About</NuxtLink></li>
+          <li><NuxtLink to="/products" class="btn">Merch</NuxtLink></li>
+        </ul>
+      </nav>
     </header>
-    <div>
-      <slot></slot>
+    <div class="container mx-auto p-4">
+      <slot />
     </div>
   </div>
 </template>
+
+<style scoped>
+.router-link-exact-active {
+  color: #12b488;
+}
+</style>
--- layouts/products.vue
+++ layouts/products.vue
@@ -1,15 +1,25 @@
 <template>
   <div>
-    <header>
-      <nav>
-        <NuxtLink to="/products">Nuxt Dojo Merch</NuxtLink>
+    <header class="shadow-sm bg-white">
+      <nav class="container mx-auto p-4">
+        <NuxtLink to="/products" class="font-bold">Nuxt Dojo Merch</NuxtLink>
       </nav>
     </header>
-    <div>
-      <slot></slot>
+    <div class="container mx-auto p-4">
+      <slot />
     </div>
-    <footer>
-      <DojoNavigation></DojoNavigation>
+    <footer class="container mx-auto p-4 flex justify-between border-t-2">
+      <ul class="flex gap-4">
+        <li><NuxtLink to="/">Home</NuxtLink></li>
+        <li><NuxtLink to="/about">About</NuxtLink></li>
+        <li><NuxtLink to="/products">Merch</NuxtLink></li>
+      </ul>
     </footer>
   </div>
 </template>
+
+<style scoped>
+.router-link-exact-active {
+  color: #12b488;
+}
+</style>
